92da57dfc950973a0538e59990153c2ef251770d
andrejmanin <andrejmanin54@gmail.com>
2024-01-13 18:40:35 +0200

    updated main.cpp

--- TaskManager/main.cpp
+++ TaskManager/main.cpp
@@ -6,16 +6,32 @@
 
 using namespace std;
 
+void show(vector<Task>& t) {
+	for(auto el : t)
+	{
+		cout << el.getTask() << endl;
+	}
+}
 
 int main() {
 	vector<Task> tasks;
 
 	string text;
+	
+	do {
+
 	cout << "Enter a task: ";
 	getline(cin, text);
-
+		if (text == "show") {
+			show(tasks);
+		}
+		else {
 	Task task(text);
 	tasks.push_back(task);
+		}
+		
+	} while (text != "stop");
+	
 
 	cout << tasks[0].getTask() << endl;
 	return 0;
